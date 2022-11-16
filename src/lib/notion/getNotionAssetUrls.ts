80a799546c9f20cc38749563e7f623fa16ee7433
John Detlefs <jdetlefs@godaddy.com>
2022-11-15 20:30:46 -0600

    Node fetch revert

--- src/lib/notion/getNotionAssetUrls.ts
+++ src/lib/notion/getNotionAssetUrls.ts
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 import { getError } from "./rpc";
 import { NextApiResponse } from "next";
 import { NOTION_TOKEN, API_ENDPOINT } from "./server-constants";
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,3 +1,4 @@
+import fetch from "node-fetch";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Header from "../../components/header";
