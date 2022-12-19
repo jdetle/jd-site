f51d79e0dd458fe276e0395355bd7165fb435aab
John Detlefs <jdetlefs@godaddy.com>
2022-12-19 09:19:51 -0600

    Remove logo

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -37,10 +37,6 @@ export default function Contact() {
     <>
       <Header titlePre="Contact" />
       <div className={sharedStyles.layout}>
-        <div className={contactStyles.avatar}>
-          <img src="/avatar.png" alt="avatar with letters JJ" height={60} />
-        </div>
-
         <h1 style={{ marginTop: 0 }}>Contact</h1>
       </div>
     </>
