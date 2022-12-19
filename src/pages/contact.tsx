cbf8aac947df0783b9b91ef3990a54e635fe7828
John Detlefs <jdetlefs@godaddy.com>
2022-12-19 09:17:41 -0600

    Remove stuff

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -42,21 +42,6 @@ export default function Contact() {
         </div>
 
         <h1 style={{ marginTop: 0 }}>Contact</h1>
-
-        <div className={contactStyles.name}>
-          JJ Kasper - Next.js Engineer @{" "}
-          <ExtLink href="https://vercel.com">Vercel</ExtLink>
-        </div>
-
-        <div className={contactStyles.links}>
-          {contacts.map(({ Comp, link, alt }) => {
-            return (
-              <ExtLink key={link} href={link} aria-label={alt}>
-                <Comp height={32} />
-              </ExtLink>
-            );
-          })}
-        </div>
       </div>
     </>
   );
