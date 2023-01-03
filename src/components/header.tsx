47998f4e2b47be7cfec9903d17576d7d81d58886
John Detlefs <jdetlefs@godaddy.com>
2023-01-02 19:13:57 -0600

    Change meta tags

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -26,7 +26,7 @@ const Header = ({ titlePre = "" }) => {
         />
         <meta name="og:title" content="My Notion Blog" />
         <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
+        <meta name="twitter:site" content="@jdetle" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
